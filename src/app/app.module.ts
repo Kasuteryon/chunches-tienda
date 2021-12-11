8e58af35f5434d891122c8827f28f0258ccb3d43
Kasuteryon <eduardo.castellon66i@std.uni.edu.ni>
2021-12-10 18:11:09 -0600

    Previous Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,7 +20,8 @@ import { FooterComponent } from './shared/components/footer/footer.component';
     BrowserAnimationsModule,
     SharedModule,
     MaterialModule,
-    ProductsModule
+    ProductsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -3,6 +3,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
@@ -27,7 +28,8 @@ import { MatFileUploadModule } from 'angular-material-fileupload';
     MatDatepickerModule,
     MatNativeDateModule,
     MatSelectModule,
-    MatFileUploadModule
+    MatFileUploadModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule {}
